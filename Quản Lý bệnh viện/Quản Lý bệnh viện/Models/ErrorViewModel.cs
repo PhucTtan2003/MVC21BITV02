07c5e637c20fc6c357abba46d4d4a92c716ef6aa
PhucTtan2003 <tan45385@gmail.com>
2024-08-02 13:46:08 +0700

    Xóa

--- Quản Lý bệnh viện/Quản Lý bệnh viện/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Quản_Lý_bệnh_viện.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
